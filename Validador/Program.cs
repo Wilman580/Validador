5a097a456bb0dc5fd1225766ed4c3e83848e17bd
Wilman580 <35231422+Wilman580@users.noreply.github.com>
2020-04-10 22:15:58 -0500

    Aumento de versatilidad
    Tomando en cuenta de que el parámetro es una cadena en la que puede llegar cualquier caracter.

--- Validador/Program.cs
+++ Validador/Program.cs
@@ -28,28 +28,44 @@ namespace Validador
         public static bool CedulaValida(string cedula)
         {
             char[] vectorCedula = cedula.ToCharArray();
+            if (vectorCedula.Length != 10)
+            {
+                return false;
+            }
             int suma = 0;
             for (int i = 0; i < vectorCedula.Length - 1; i++)
             {
                 if ((i % 2) == 0)
                 {
-                    suma += Producto(2, Int32.Parse(vectorCedula[i].ToString()));
+                    try
+                    {
+                        suma += Producto(2, Int32.Parse(vectorCedula[i].ToString()));
+                    }
+                    catch { return false; }
                 }
                 else
                 {
-                    suma += Producto(1, Int32.Parse(vectorCedula[i].ToString()));
+                    try
+                    {
+                        suma += Producto(1, Int32.Parse(vectorCedula[i].ToString()));
+                    }
+                    catch { return false; }
                 }
             }
             int modulo = suma % 10;
-            if (modulo == 0 && (Int32.Parse(vectorCedula[9].ToString())) ==0)
+            try
             {
-                return true;
+                if (modulo == 0 && (Int32.Parse(vectorCedula[9].ToString())) == 0)
+                {
+                    return true;
+                }
+                int sup = (10 - modulo) + suma;
+                if ((Int32.Parse(vectorCedula[9].ToString())) == (sup - suma))
+                {
+                    return true;
+                }
             }
-            int sup = (10 - modulo) + suma;
-            if ((Int32.Parse(vectorCedula[9].ToString())) == (sup - suma))
-            {
-                return true;
-            }
+            catch { return false; }
             return false;
 
         }
